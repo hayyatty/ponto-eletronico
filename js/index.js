7279b2379441e3a734fac3f14ca46583dc72d2ba
Lucas Vieira Porto <lucas.vporto@dinfor.uniceub>
2024-09-03 08:51:02 -0300

    aba de bater ponto

--- js/index.js
+++ js/index.js
@@ -2,17 +2,19 @@ const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dias-mes-ano");
 const horaMinSeg = document.getElementById("hora-min-seg");
 
-const date = new Date();
+const btnBaterPonto = document.getElementById("btn-bater-ponto");
+btnBaterPonto.addEventListener("click", register)
+
+
+
+const dialogPonto = document.getElementById("dialog-ponto");
+
 
 diaSemana.textContent = getWeekDay();
 diaMesAno.textContent = getCurrentDate();
 horaMinSeg.textContent = getCurrentHour(); 
 
 
-function printCurrentHour(){
-    horaMinSeg.textContent = getCurrentHour();
-
-}
 function getWeekDay(){
     const date = new Date();
     const diaDaSemana = new Map ([
@@ -23,40 +25,53 @@ function getWeekDay(){
         [4, "Quinta"],
         [5, "Sexta"],
         [6, "Sabado"]
-    
+        
     ]);
     return diaDaSemana.get(date.getDay());
 }
 function getCurrentHour() {
     const date = new Date();
-
+    
     let hora = date.getHours();
     let min = date.getMinutes();
     let seg = date.getSeconds();
-
+    
     if (hora < 10) {
         hora = "0"+hora
-    }
+    };
     if (min < 10) {
         min = "0"+min
-    }
+    };
     if (seg < 10) {
         seg = "0"+seg
-    }
-    return hora + ":" + min + ":" + seg 
+    };
+    return hora + ":" + min + ":" + seg;
     
 }
+function printCurrentHour(){
+    horaMinSeg.textContent = getCurrentHour();
+
+}
 function getCurrentDate() {
-    // alterar solucao para que ela 
     const date = new Date();
     const dia = date.getDate();
+    const ano = date.getFullYear();
     let mes = date.getMonth();
     if (mes < 10) {
         mes = "0" + (mes + 1 )
-    }
+    };
 
-    return date.toLocaleDateString("en-US", dia + mes + date.getFullYear());
+    return dia + "/" + mes + "/" + ano;
     
 }
 
 setInterval(printCurrentHour, 1000);
+
+
+function register() {
+    dialogPonto.showModal();
+};
+
+function exit() {
+    dialogPonto.close();
+}
